77eaea1a2b18ecb14e2fc1738e9fe25f31ac971f
Tracy Karanja <chirutracy@gmail.com>
2024-03-17 11:26:05 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//Close navbar menu when a link is clicked (for responsive design)
+//document.querySelectorAll('nav a').forEach(anchor => {
+//    anchor.addEventListener('click', function (e) {
+//      e.preventDefault();
+//      document.querySelector(this.getAttribute('href')).scrollIntoView({
+//        behavior: 'smooth'
+//      });
+//    });
+//  });
+//  
